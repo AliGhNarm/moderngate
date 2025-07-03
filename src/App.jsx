30aee955ab143e930f3b597edcbb8e784caa42cd
razaaliwebdev <razaali.webdev@gmail.com>
2025-07-03 13:12:30 +0500

    setup the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Services from "./pages/Services";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div className="text-primary">App</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/our-services" element={<Services />} />
+        <Route path="/contact-us" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
